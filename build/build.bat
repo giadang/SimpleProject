5e65737068ca343e464b5fdc46536b75efd52a50
Hoang Gia Dang <Hoang Gia Dang>
2016-01-13 17:46:06 +0700

    Add submodule opencv contrib

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,3 @@
 set CMAKE_BIN_DIR="C:/Program Files (x86)/CMake/bin/cmake.exe"
-call %CMAKE_BIN_DIR% ../
+call %CMAKE_BIN_DIR% ../ -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules 
 call SP.sln
